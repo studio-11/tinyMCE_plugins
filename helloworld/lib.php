7310a2e7ab11e87354134f90e0f540f4eeef6d03
studio-11 <131365144+studio-11@users.noreply.github.com>
2025-05-06 12:06:58 +0200

    go

--- helloworld/lib.php
+++ helloworld/lib.php
@@ -1,7 +1,14 @@
 <?php
+defined('MOODLE_INTERNAL') || die();
+
 /**
- * Initialise the Atto HelloWorld button.
+ * Atto HelloWorld – aucune donnée à transmettre au JS.
+ *
+ * @param string $elementid   L’ID du champ textarea/iframe.
+ * @param int    $contextid   Contexte, non utilisé ici.
+ * @param array  $options     Options de l’éditeur.
+ * @return array              Paramètres envoyés au module YUI.
  */
-function atto_helloworld_params_for_js(\editor_atto\editor $editor, int $contextid, array $options) : array {
-    return []; // Pas de paramètre côté JS
+function atto_helloworld_params_for_js($elementid, $contextid, $options) : array {
+    return [];
 }
--- helloworld/version.php
+++ helloworld/version.php
@@ -1,5 +1,9 @@
+<?php
 defined('MOODLE_INTERNAL') || die();
 
+$plugin = new stdClass();          // ←  important !
 $plugin->component = 'atto_helloworld';
-$plugin->version   = 2025050600;
-$plugin->requires  = 2022112800;     // Moodle 4.1 min.
+$plugin->version = 2025050601;   // ← +1
+$plugin->requires  = 2022112800;   // version Moodle mini, termine par ;
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = '0.1.0';
