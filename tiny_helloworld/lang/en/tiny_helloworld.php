8e4083382972bf5fb44d149dc0d7ea2ab604b1d0
studio-11 <131365144+studio-11@users.noreply.github.com>
2025-08-07 13:51:39 +0200

    Delete tiny_helloworld directory

--- tiny_helloworld/lang/en/tiny_helloworld.php
+++
@@ -1,4 +0,0 @@
-<?php
-$string['pluginname'] = 'Hello World (TinyMCE)';
-$string['button_helloworld'] = 'Hello World';
-$string['privacy:metadata'] = 'The Hello World plugin stores no personal data.';
