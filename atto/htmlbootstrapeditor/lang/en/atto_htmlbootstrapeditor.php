379bbaeb4c6953dd7f2b3b3bd7bb54aa36a08a8b
studio-11 <131365144+studio-11@users.noreply.github.com>
2025-08-07 13:54:13 +0200

    Delete atto directory

--- atto/htmlbootstrapeditor/lang/en/atto_htmlbootstrapeditor.php
+++
@@ -1,4 +0,0 @@
-<?php
-$string['pluginname'] = 'HTML Bootstrap Editor Atto';
-$string['htmleditor'] = 'HTML Bootstrap Editor';
-$string['privacy:metadata'] = 'The atto_htmlbootstrapeditor plugin does not store user data.';
