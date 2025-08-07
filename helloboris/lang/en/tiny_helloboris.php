40ee880f9701ba6a82b273781d07f2120228c0ff
studio-11 <131365144+studio-11@users.noreply.github.com>
2025-08-07 13:54:03 +0200

    Delete helloboris directory

--- helloboris/lang/en/tiny_helloboris.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-
-$string['buttontitle']       = 'Boris';
-$string['pluginname'] = 'Hello Boris';
-$string['privacy:metadata']  = 'The Inspirational Quotes plugin does not store any personal data.';
